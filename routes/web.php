a02b58e3ae0b8c0949e9963e49c54346b0ffb05c
Justijn Depover <justijndepover@gmail.com>
2022-04-27 13:20:12 +0200

    test route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('nick', function () {
+    return "Hello Nick";
+});
+
 Route::get('phpinfo', function () {
     phpinfo();
 });
